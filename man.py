6d0519bc79091aee5e150b3fa1562ed94532b49e
TREITH2006 <adi867371@gmail.com>
2026-03-20 16:28:11 +0530

    this is a code which is written by author as a example

--- man.py
+++ man.py
@@ -28,3 +28,34 @@ b2=bankaccount("sai", 2000)
 b2.show()
 
 #the 3rd code 
+class Zoo:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def animal(self):
+        print(f"The name of the animal is {self.name} and the age is {self.age}")
+
+    def bird(self):
+        print(f"The name of the bird is {self.name} and the age is {self.age}")
+
+
+print("Welcome to the zoo")
+print("1. Animals\n2. Birds")
+
+choice = int(input("Enter your choice: "))
+
+if choice == 1:
+    name = input("Enter animal name: ")
+    age = int(input("Enter animal age: "))
+    obj = Zoo(name, age)
+    obj.animal()
+
+elif choice == 2:
+    name = input("Enter bird name: ")
+    age = int(input("Enter bird age: "))
+    obj = Zoo(name, age)
+    obj.bird()
+
+else:
+    print("Invalid choice")
